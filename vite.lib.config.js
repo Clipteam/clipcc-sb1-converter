d297c49f4e20037c397e9039cdda36d72c2db1c6
SimonShiki <sinangentoo@gmail.com>
2026-03-12 09:03:07 +0800

    :wrench: chore: use cjs for node build
    Signed-off-by: SimonShiki <sinangentoo@gmail.com>

--- vite.lib.config.js
+++ vite.lib.config.js
@@ -9,8 +9,8 @@ export default defineConfig({
         lib: {
             entry: path.resolve(__dirname, 'index.js'),
             name: 'sb1Converter',
-            fileName: format => (format === 'umd' ? 'main.js' : `main.${format}.js`),
-            formats: ['umd']
+            fileName: format => (format === 'cjs' ? 'main.js' : `main.${format}.js`),
+            formats: ['cjs']
         }
     },
     plugins: [
